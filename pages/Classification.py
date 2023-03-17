09e61112cedb5190958c916220fba84303ef7dd7
Vsevolod Troyanov <119865130+trojanof@users.noreply.github.com>
2023-03-17 20:20:11 +0500

    Add files via upload

--- pages/Classification.py
+++ pages/Classification.py
@@ -14,10 +14,84 @@ from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 import torch.nn as nn
-from rnn_preprocessing import padding, data_preprocessing, preprocess_single_string
+#from rnn_preprocessing import padding, data_preprocessing, preprocess_single_string
+
+import re
+import string
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
+st.set_page_config(layout='wide')
+
+def data_preprocessing(text: str) -> str:
+    """preprocessing string: lowercase, removing html-tags, punctuation and stopwords
+
+    Args:
+        text (str): input string for preprocessing
+
+    Returns:
+        str: preprocessed string
+    """    
+
+    text = text.lower()
+    text = re.sub('<.*?>', '', text) # html tags
+    text = ''.join([c for c in text if c not in string.punctuation])# Remove punctuation
+    text = [word for word in text.split() if word not in stop_words] 
+    text = ' '.join(text)
+    return text
+
+def get_words_by_freq(sorted_words: list, n: int = 10) -> list:
+    return list(filter(lambda x: x[1] > n, sorted_words))
+
+def padding(review_int: list, seq_len: int) -> np.array:
+    """Make left-sided padding for input list of tokens
+
+    Args:
+        review_int (list): input list of tokens
+        seq_len (int): max length of sequence, it len(review_int[i]) > seq_len it will be trimmed, else it will be padded by zeros
+
+    Returns:
+        np.array: padded sequences
+    """    
+    features = np.zeros((len(review_int), seq_len), dtype = int)
+    for i, review in enumerate(review_int):
+        if len(review) <= seq_len:
+            zeros = list(np.zeros(seq_len - len(review)))
+            new = zeros + review
+        else:
+            new = review[: seq_len]
+        features[i, :] = np.array(new)
+            
+    return features
+
+def preprocess_single_string(
+    input_string: str, 
+    seq_len: int, 
+    vocab_to_int: dict,
+    ) -> torch.tensor:
+    """Function for all preprocessing steps on a single string
+
+    Args:
+        input_string (str): input single string for preprocessing
+        seq_len (int): max length of sequence, it len(review_int[i]) > seq_len it will be trimmed, else it will be padded by zeros
+        vocab_to_int (dict, optional): word corpus {'word' : int index}. Defaults to vocab_to_int.
+
+    Returns:
+        list: preprocessed string
+    """    
+
+    preprocessed_string = data_preprocessing(input_string)
+    result_list = []
+    for word in preprocessed_string.split():
+        try: 
+            result_list.append(vocab_to_int[word])
+        except KeyError as e:
+            print(f'{e}: not in dictionary!')
+    result_padded = padding([result_list], seq_len)[0]
+
+    return torch.tensor(result_padded)
 
 
-#st.set_page_config(layout='wide')  # если поставить, страница будет всегда по макс. ширине окна
 
 device = 'cpu'
 
@@ -119,9 +193,6 @@ with col1:
     msg_df.columns = vec_from_disk.get_feature_names_out()
     probability = mdl_from_disk.predict_proba(msg_df)[0][1]
 
-        #
-    #тут собственно передаешь текст в векторизатор, в кэтбуст и т.д и сохраняешь аутпут 
-    #
     if probability > 0.5:
         output = 'POSITIVE'
     else:
@@ -148,9 +219,7 @@ with col2:
         output = 'POSITIVE'
     else:
         output = 'NEGATIVE'
-    #
-    #тут передаешь текст в модель и  сохраняешь аутпут 
-    #
+    
     if button_check:
         st.markdown(f'`{output}`')
 
@@ -165,10 +234,7 @@ with col3:
         [здесь](https://huggingface.co/blanchefort/rubert-base-cased-sentiment)
     ''')
         
-    #
-    #тут передаешь текст в модель и  сохраняешь аутпут 
-    #
-
+    
     API_TOKEN = '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'
     API_URL = "https://api-inference.huggingface.co/models/blanchefort/rubert-base-cased-sentiment"
     headers = {"Authorization": f"Bearer {API_TOKEN}"}
